4226616db3fa502cc33e020290863dacec07e9d7
aigamo <aigamosan@gmail.com>
2016-10-28 12:07:42 +0900

    add initial spring-boot package

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # TheFourSamurai
 テクのこ2016年ハッカソンアプリ
+
+
+## 作り方
+
+ビルドインストール
+
+	mvn clean install
+	
+Eclipseプロジェクト化
+
+	mvn eclipse:clean eclipse:eclipse
+
+Spring-boot起動
+	
+	mvn spring-boot:run
---
+++ src/main/java/jp/technoco/thefoursamurai/TheFourSamuraiApplication.java
@@ -0,0 +1,12 @@
+package jp.technoco.thefoursamurai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFourSamuraiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheFourSamuraiApplication.class, args);
+    }
+}
